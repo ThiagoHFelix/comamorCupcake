b23f0b443ac64a8f98a9fdb842553685bc865b9f
Thiago Henrique Felix <thiagofelix.on@gmail.com>
2018-12-02 14:38:40 -0200

    -

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -2,7 +2,7 @@
   <f7-page color-theme="pink">
     <f7-navbar title="Menu"></f7-navbar>
     <f7-list>
-      <f7-list-item link="/encomendas/" title="Emcomendas" view="#main-view" panel-close></f7-list-item>
+      <f7-list-item link="/encomendas/" title="Encomendas" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/cupcakes/" title="Cupcakes" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/empresa/" title="Empresa" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/contato/" title="Contato" view="#main-view" panel-close></f7-list-item>
